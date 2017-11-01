32cfb93257879c09bb8fd65f4ef5c3129c0417be
Max Harper <maxharp3r@gmail.com>
2017-11-01 10:08:15 -0500

    more empty

--- web/src/App.js
+++ web/src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
+// import { Route } from 'react-router-dom'
 
 class App extends Component {
 
+  /*
+  <section className="section">
+    <div className="content">
+    </div>
+  </section>
+  */
   render() {
 
     return (
       <div className="App">
-        <section className="section">
-          <div className="content">
-
-            <h1>hello, world</h1>
-
-          </div>
-        </section>
+        hello, world
       </div>
     );
   }
